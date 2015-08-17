cf520ab8d3222d411fac853fd12dbe7f7f24e14f
Temirlan <tanuku07@gmail.com>
2015-08-17 23:39:45 +0600

    Bit customized toolbar

--- app/src/main/java/kg/android/instagram/activities/MainActivity.java
+++ app/src/main/java/kg/android/instagram/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package kg.android.instagram.activities;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
